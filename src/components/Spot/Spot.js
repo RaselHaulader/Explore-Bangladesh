b93267bdcb05f0e60b346d6b0d09ca00a42a0420
Rasel <hauladerrasel2@gmail.com>
2021-09-26 22:57:19 +0600

    final push

--- src/components/Spot/Spot.js
+++ src/components/Spot/Spot.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Spot.css'
 
 const Spot = (props) => {
+    
     const { spot, img, location, duration, cost, distance } = props.spot
     return (
         <div className="col" >
             <div className="card h-100 spot">
-                <img src={img}  className="card-img-top" alt="..." />
+                <div className="overflow-hidden w-100">
+                <img src={img} className="card-img-top" alt="..." />
+                </div>
                 <div className="card-body d-flex flex-column justify-content-between">
                     <div className="spot-info">
                         <h4 className="card-title">{spot}</h4>
@@ -16,7 +19,7 @@ const Spot = (props) => {
                         <p><i className="fas fa-coins"></i> {cost} BDT</p>
                     </div>
                     <div>
-                        <button className="add-btn" onClick={()=>props.handleTotal(props.spot)}>Add to wishlist  <i className="fas fa-heart heart-icon"></i></button>
+                        <button className="add-btn" onClick={() => props.handleTotal(props.spot)}>Add to wishlist  <i className="fas fa-heart heart-icon"></i></button>
                     </div>
                 </div>
             </div>
